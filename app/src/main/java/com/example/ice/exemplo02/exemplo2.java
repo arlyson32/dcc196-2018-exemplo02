dd3ae3331d5b08b5302784314e894bda897349bc
arlyson <arlyson32@gmail.com>
2018-08-29 20:03:15 -0300

    teste de pagina professor

--- app/src/main/java/com/example/ice/exemplo02/exemplo2.java
+++ app/src/main/java/com/example/ice/exemplo02/exemplo2.java
@@ -2,12 +2,26 @@ package com.example.ice.exemplo02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class exemplo2 extends AppCompatActivity {
+
+    Button btnProfessor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exemplo2);
+        btnProfessor = findViewById(R.id.btn_professor);
+        btnProfessor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(exemplo2.this,ProfessorActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
