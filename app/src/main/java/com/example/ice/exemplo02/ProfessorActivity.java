73fdd13d60510c58c8354b35233f493ce003e8a6
arlyson <arlyson32@gmail.com>
2018-08-29 20:14:49 -0300

    Cria intente da active para o professor active

--- app/src/main/java/com/example/ice/exemplo02/ProfessorActivity.java
+++ app/src/main/java/com/example/ice/exemplo02/ProfessorActivity.java
@@ -2,12 +2,23 @@ package com.example.ice.exemplo02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
 
 public class ProfessorActivity extends AppCompatActivity {
+    private TextView txtProfMensagem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_professor);
+        txtProfMensagem = findViewById(R.id.txt_prof_mensagem);
+        Bundle extras = getIntent().getExtras();
+        if(extras != null){
+            String nome = extras.getString("nome");
+            txtProfMensagem.setText("Olá Pro. "+nome+ "!");
+        }
     }
 }
--- app/src/main/java/com/example/ice/exemplo02/exemplo2.java
+++ app/src/main/java/com/example/ice/exemplo02/exemplo2.java
@@ -4,21 +4,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.content.Intent;
 
 public class exemplo2 extends AppCompatActivity {
 
-    Button btnProfessor;
+    private Button btnProfessor;
+    private EditText edtNome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exemplo2);
         btnProfessor = findViewById(R.id.btn_professor);
+        edtNome = findViewById(R.id.edtNome);
         btnProfessor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(exemplo2.this,ProfessorActivity.class);
+                intent.putExtra("nome", edtNome.getText().toString());
                 startActivity(intent);
             }
         });
